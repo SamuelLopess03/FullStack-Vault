3321ebef4e9721ccd35779abdc77e1fd2a7ecaec
Samuel Lopes <samuellopes12390@gmail.com>
2025-09-03 18:42:53 -0300

    [Money_Manager] feat(backend-controllers): Criado a Classe ExpenseController com Endpoints de Criação e Consulta
    Implementado métodos (endpoints) de adição de Expense e consulta
    de Expenses filtrando pelo último mês.

---
+++ Money_Manager/backend/src/main/java/com/samuel/moneymanager/controllers/ExpenseController.java
@@ -0,0 +1,39 @@
+package com.samuel.moneymanager.controllers;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.samuel.moneymanager.dtos.ExpenseDTO;
+import com.samuel.moneymanager.services.ExpenseService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/expenses")
+public class ExpenseController {
+	
+	private final ExpenseService expenseService;
+	
+	@PostMapping
+	public ResponseEntity<ExpenseDTO> addExpense(@RequestBody ExpenseDTO dto) {
+		ExpenseDTO saved = expenseService.addExpense(dto);
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(saved);
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<ExpenseDTO>> getExpenses() {
+		List<ExpenseDTO> expenses = expenseService.getCurrentMonthExpensesForCurrentUser();
+		
+		return ResponseEntity.ok(expenses);
+	}
+	
+}
