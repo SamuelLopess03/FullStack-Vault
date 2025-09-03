dc374dd1e492b314a9836fb30637666a9a64320c
Samuel Lopes <samuellopes12390@gmail.com>
2025-09-03 18:44:14 -0300

    [Money_Manager] feat(backend-controllers): Criado a Classe IncomeController com Endpoints de Criação e Consulta
    Implementado métodos (endpoints) de adição de Income e consulta
    de Incomes filtrando pelo último mês.

---
+++ Money_Manager/backend/src/main/java/com/samuel/moneymanager/controllers/IncomeController.java
@@ -0,0 +1,39 @@
+package com.samuel.moneymanager.controllers;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.samuel.moneymanager.dtos.IncomeDTO;
+import com.samuel.moneymanager.services.IncomeService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/incomes")
+public class IncomeController {
+
+	private final IncomeService incomeService;
+	
+	@PostMapping
+	public ResponseEntity<IncomeDTO> addIncome(@RequestBody IncomeDTO dto) {
+		IncomeDTO saved = incomeService.addIncome(dto);
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(saved);
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<IncomeDTO>> getIncomes() {
+		List<IncomeDTO> incomes = incomeService.getCurrentMonthIncomesForCurrentUser();
+		
+		return ResponseEntity.ok(incomes);
+	}
+	
+}
