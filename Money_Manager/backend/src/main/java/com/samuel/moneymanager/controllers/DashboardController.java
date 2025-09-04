bef102ea93cfa04f21727fdc79a1a7aa1008cfda
Samuel Lopes <samuellopes12390@gmail.com>
2025-09-04 18:47:19 -0300

    [Money_Manager] feat(backend-controllers): Estruturado o DashboardController com Endpoint de Consulta de Dados

---
+++ Money_Manager/backend/src/main/java/com/samuel/moneymanager/controllers/DashboardController.java
@@ -0,0 +1,27 @@
+package com.samuel.moneymanager.controllers;
+
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.samuel.moneymanager.services.DashboardService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/dashboard")
+public class DashboardController {
+	
+	private final DashboardService dashboardService;
+	
+	@GetMapping
+	public ResponseEntity<Map<String, Object>> getDashboardData() {
+		Map<String, Object> dashboardData = dashboardService.getDashboardData();
+		
+		return ResponseEntity.ok(dashboardData);
+	}
+}
