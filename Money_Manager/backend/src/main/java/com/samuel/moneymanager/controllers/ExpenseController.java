21acd664ce196af841cc07be2e5e0cdce2025540
Samuel Lopes <samuellopes12390@gmail.com>
2025-09-04 18:43:44 -0300

    [Money_Manager] feat(backend-controllers): Adicionado o Endpoint de Deleção para Expense

--- Money_Manager/backend/src/main/java/com/samuel/moneymanager/controllers/ExpenseController.java
+++ Money_Manager/backend/src/main/java/com/samuel/moneymanager/controllers/ExpenseController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,6 +36,13 @@ public class ExpenseController {
 		List<ExpenseDTO> expenses = expenseService.getCurrentMonthExpensesForCurrentUser();
 		
 		return ResponseEntity.ok(expenses);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteExpense(@PathVariable Long id) {
+		expenseService.deleteExpense(id);
+		
+		return ResponseEntity.noContent().build();
 	}
 	
 }
