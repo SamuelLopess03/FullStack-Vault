3ef364eacfca9760dd2e32bd31b07eaa8b490524
Samuel Lopes <samuellopes12390@gmail.com>
2025-09-05 18:51:02 -0300

    [Money_Manager] refactor(backend-services): Alterado o Envio de Email do SimpleMailMessage para MimeMessage
    Tal alteração é necessária para enviar estrutura de HTML
    no email.

--- Money_Manager/backend/src/main/java/com/samuel/moneymanager/services/EmailService.java
+++ Money_Manager/backend/src/main/java/com/samuel/moneymanager/services/EmailService.java
@@ -1,10 +1,11 @@
 package com.samuel.moneymanager.services;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -17,17 +18,18 @@ public class EmailService {
 	private String fromEmail;
 	
 	public void sendEmail(String to, String subject, String body) {
-		try {
-			SimpleMailMessage message = new SimpleMailMessage();
-			
-			message.setFrom(fromEmail);
-			message.setTo(to);
-			message.setSubject(subject);
-			message.setText(body);
-			
-			mailSender.send(message);
-		} catch (Exception e) {
-			throw new RuntimeException(e.getMessage());
-		}
-	}
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+
+            helper.setFrom(fromEmail);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(body, true);
+
+            mailSender.send(message);
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao enviar e-mail: " + e.getMessage());
+        }
+    }
 }
