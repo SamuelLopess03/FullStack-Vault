248ff6e1f36a6839e0512dcba0ff80a484faf3ff
Samuel Lopes <samuellopes12390@gmail.com>
2025-09-12 16:49:37 -0300

    [Money_Manager] feat(frontend-app): Roteado as Páginas do Projeto em App
    Além disso, foi necessário baixar a dependência
    react-hot-toast para adicionar o componente
    Toaster em App.

--- Money_Manager/frontend/src/App.jsx
+++ Money_Manager/frontend/src/App.jsx
@@ -1,7 +1,30 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+
+import Home from "./pages/Home";
+import Income from "./pages/Income";
+import Expense from "./pages/Expense";
+import Category from "./pages/Category";
+import Filter from "./pages/Filter";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Money Manager</h1>
+      <Toaster />
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/dashboard" element={<Home />} />
+          <Route path="/income" element={<Income />} />
+          <Route path="/expense" element={<Expense />} />
+          <Route path="/category" element={<Category />} />
+          <Route path="/filter" element={<Filter />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
