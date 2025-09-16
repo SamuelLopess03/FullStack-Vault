a9ae63b779e0a05eea2cc46bd32a828e3f41ca9f
Samuel Lopes <samuellopes12390@gmail.com>
2025-09-15 21:53:07 -0300

    [Money_Manager] feat(frontend-pages): Desenvolvido a Estrutura da Página de Login
    Com o envio da requisição de Login é necessário
    armazenar os dados do usuário de forma global,
    então com isso foi utilizado o contexto AppContext
    da aplicação.

--- Money_Manager/frontend/src/pages/Login.jsx
+++ Money_Manager/frontend/src/pages/Login.jsx
@@ -1,5 +1,146 @@
+import { useContext, useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { LoaderCircle } from "lucide-react";
+
+import { assets } from "../assets/assets";
+
+import { validateEmail } from "../utils/validation";
+import axiosConfig from "../utils/axiosConfig";
+import { API_ENDPOINTS } from "../utils/apiEndpoints";
+
+import Input from "../components/Input";
+import { AppContext } from "../context/AppContext";
+
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { setUser } = useContext(AppContext);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    if (!validateEmail(email)) {
+      setError("Please enter valid email address");
+      setIsLoading(false);
+
+      return;
+    }
+
+    if (!password.trim()) {
+      setError("Please enter your password");
+      setIsLoading(false);
+
+      return;
+    }
+
+    setError("");
+
+    try {
+      const response = await axiosConfig.post(API_ENDPOINTS.LOGIN, {
+        email,
+        password,
+      });
+
+      const { token, user } = response.data;
+
+      if (token) {
+        localStorage.setItem("token", token);
+
+        setUser(user);
+
+        navigate("/dashboard");
+      }
+    } catch (error) {
+      if (error.response && error.response.data.message) {
+        setError(error.response.data.message);
+      } else {
+        console.error("Something went wrong", error);
+        setError(error.message);
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div className="h-screen w-full relative flex items-center justify-center overflow-hidden">
+      <img
+        src={assets.login_bg}
+        alt="Background"
+        className="absolute inset-0 w-full h-full object-cover filter blur-sm"
+      />
+
+      <div className="relative z-10 w-full max-w-lg px-6">
+        <div className="bg-white bg-opacity-95 backdrop-blur-sm rounded-lg shadow-2xl p-8 max-h-[90vh] overflow-y-auto">
+          <h3 className="text-2xl font-semibold text-black text-center mb-2">
+            Welcome Back
+          </h3>
+
+          <p className="text-sm text-slate-700 text-center mb-8">
+            Please enter your details to login in
+          </p>
+
+          <form onSubmit={handleSubmit} className="space-y-4">
+            <Input
+              type="email"
+              label="Email Address"
+              placeholder="name@example.com"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+            />
+
+            <Input
+              type="password"
+              label="Password"
+              placeholder="**********"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+            />
+
+            {error && (
+              <p className="text-red-800 text-sm text-center bg-red-50 p-2 rounded">
+                {error}
+              </p>
+            )}
+
+            <button
+              className={`btn-primary w-full py-2 text-lg font-medium cursor-pointer flex items-center 
+                justify-center gap-2 ${
+                  isLoading ? "opacity-60 cursor-not-allowed" : ""
+                }`}
+              type="submit"
+              disabled={isLoading}
+            >
+              {isLoading ? (
+                <>
+                  <LoaderCircle className="animate-spin w-5 h-5" />
+                  Logging in...
+                </>
+              ) : (
+                "LOGIN"
+              )}
+            </button>
+
+            <p className="text-sm text-slate-800 text-center mt-3">
+              Don't have an account?
+              <Link
+                to="/signup"
+                className="font-medium text-primary underline hover:text-primary-dark transition-colors"
+              >
+                Signup
+              </Link>
+            </p>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
