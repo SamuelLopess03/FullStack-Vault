830645a7caedbc1199f19c0a6097da82dd058bbd
Samuel Lopes <samuellopes12390@gmail.com>
2025-09-17 14:27:42 -0300

    [Money_Manager] feat(frontend-utils): Desenvolvido um Método Utilitário que Salva a Imagem Anexada pelo Usuário no Cloudinary

--- Money_Manager/frontend/src/utils/apiEndpoints.js
+++ Money_Manager/frontend/src/utils/apiEndpoints.js
@@ -1,6 +1,8 @@
 export const BASE_URL = "https://money-manager-cbp2.onrender.com/api/v1.0";
+const CLOUDINARY_CLOUD_NAME = "dixnhc2vb";
 
 export const API_ENDPOINTS = {
   LOGIN: "/login",
   REGISTER: "/register",
+  UPLOAD_IMAGE: `https://api.cloudinary.com/v1_1/${CLOUDINARY_CLOUD_NAME}/image/upload`,
 };
