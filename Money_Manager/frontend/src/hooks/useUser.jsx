3adc4c82370114f4dee80e025271070c03b437e3
Samuel Lopes <samuellopes12390@gmail.com>
2025-09-18 17:23:13 -0300

    [Money_Manager] feat(frontend-hooks): Desenvolvido Arquivo que Utiliza os Hooks para o Estado de User

---
+++ Money_Manager/frontend/src/hooks/useUser.jsx
@@ -0,0 +1,44 @@
+import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+import axiosConfig from "../utils/axiosConfig";
+import { API_ENDPOINTS } from "../utils/apiEndpoints";
+
+import { AppContext } from "../context/AppContext";
+
+export const useUser = () => {
+  const { user, setUser, clearUser } = useContext(AppContext);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user) {
+      return;
+    }
+
+    let isMounted = true;
+
+    const fetchUserInfo = async () => {
+      try {
+        const response = await axiosConfig.get(API_ENDPOINTS.GET_USER_INFO);
+
+        if (isMounted && response.data) {
+          setUser(response.data);
+        }
+      } catch (error) {
+        console.error("Failed to Fetch the User Info", error);
+
+        if (isMounted) {
+          clearUser();
+          navigate("/login");
+        }
+      }
+    };
+
+    fetchUserInfo();
+
+    return () => {
+      isMounted = false;
+    };
+  }, [setUser, clearUser, navigate]);
+};
