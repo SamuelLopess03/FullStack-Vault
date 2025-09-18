76d6bcc66933298afbebca48196e50dfc9d1ce2e
Samuel Lopes <samuellopes12390@gmail.com>
2025-09-18 17:41:35 -0300

    [Money_Manager] feat(frontend-context): Desenvolvido Função que Limpa o Estado do Usuário no Contexto Global

--- Money_Manager/frontend/src/context/AppContext.jsx
+++ Money_Manager/frontend/src/context/AppContext.jsx
@@ -5,7 +5,11 @@ export const AppContext = createContext();
 export const AppContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
 
-  const contextValue = { user, setUser };
+  const clearUser = () => {
+    setUser(null);
+  };
+
+  const contextValue = { user, setUser, clearUser };
 
   return (
     <AppContext.Provider value={contextValue}>{children}</AppContext.Provider>
