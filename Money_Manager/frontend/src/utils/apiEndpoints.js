cb51d5a6b8aae7249b5c7c194914359bd17bec46
Samuel Lopes <samuellopes12390@gmail.com>
2025-09-21 08:37:34 -0300

    [Money_Manager] feat(frontend-utils): Acrescentado Mapeamento de Outros Endpoints da API do Backend em apiEndpoints

--- Money_Manager/frontend/src/utils/apiEndpoints.js
+++ Money_Manager/frontend/src/utils/apiEndpoints.js
@@ -1,8 +1,11 @@
 export const BASE_URL = "https://money-manager-cbp2.onrender.com/api/v1.0";
+
 const CLOUDINARY_CLOUD_NAME = "dixnhc2vb";
 
 export const API_ENDPOINTS = {
   LOGIN: "/login",
   REGISTER: "/register",
+  GET_USER_INFO: "/profile",
+  GET_ALL_CATEGORIES: "/categories",
   UPLOAD_IMAGE: `https://api.cloudinary.com/v1_1/${CLOUDINARY_CLOUD_NAME}/image/upload`,
 };
