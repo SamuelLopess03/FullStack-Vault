261740813723362d633c1e907d6c52c6e62cb35e
Samuel Lopes <samuellopes12390@gmail.com>
2025-10-01 17:43:22 -0300

    [Money_Manager] feat(backend-services): Implementado o Método que Realiza o Envio do Email com Arquivo Excel

--- Money_Manager/backend/src/main/java/com/samuel/moneymanager/services/EmailService.java
+++ Money_Manager/backend/src/main/java/com/samuel/moneymanager/services/EmailService.java
@@ -1,10 +1,12 @@
 package com.samuel.moneymanager.services;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
+import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 
@@ -32,4 +34,18 @@ public class EmailService {
             throw new RuntimeException("Erro ao enviar e-mail: " + e.getMessage());
         }
     }
+	
+	public void sendEmailWithAttachment(String to, String subject, String body, byte[] attachment, String filename) throws MessagingException {
+		MimeMessage message = mailSender.createMimeMessage();
+		MimeMessageHelper helper = new MimeMessageHelper(message, true);
+		
+		helper.setFrom(fromEmail);
+		helper.setTo(to);
+		helper.setSubject(subject);
+		helper.setText(body);
+		helper.addAttachment(filename, new ByteArrayResource(attachment));
+		
+		mailSender.send(message);
+	}
+	
 }
