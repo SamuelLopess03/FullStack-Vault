916e490ba929955872d0f31c13111a6ff8b5eff5
Samuel Lopes <samuellopes12390@gmail.com>
2025-10-13 16:24:30 -0300

    [Money_Manager] feat(components): Desenvolvido o Componente que Exibi Todos os Incomes Cadastrados no Mês Pelo Usuário

--- Money_Manager/frontend/src/components/IncomeList.jsx
+++ Money_Manager/frontend/src/components/IncomeList.jsx
@@ -1,22 +1,67 @@
-import { Download, Mail } from "lucide-react";
+import { useState } from "react";
+import { Download, LoaderCircle, Mail } from "lucide-react";
 import moment from "moment";
 
 import TransactionInfoCard from "./TransactionInfoCard";
 
-const IncomeList = ({ transactions, onDelete }) => {
+const IncomeList = ({ transactions, onDelete, onDownload, onEmail }) => {
+  const [loading, setLoading] = useState(false);
+
+  const handleEmail = async () => {
+    setLoading(true);
+
+    try {
+      await onEmail();
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleDownload = async () => {
+    setLoading(true);
+
+    try {
+      await onDownload();
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="card p-5">
       <div className="flex items-center justify-between">
         <h5 className="text-lg font-semibold">Income Sources</h5>
 
         <div className="flex items-center justify-end gap-2">
-          <button className="card-btn">
-            <Mail size={15} className="text-base" />{" "}
-            <span className="text-sm font-medium">Email</span>
+          <button disabled={loading} className="card-btn" onClick={handleEmail}>
+            {loading ? (
+              <>
+                <LoaderCircle className="w-4 h-4 animate-spin" />
+                Emailing...
+              </>
+            ) : (
+              <>
+                <Mail size={15} className="text-base" />{" "}
+                <span className="text-sm font-medium">Email</span>
+              </>
+            )}
           </button>
-          <button className="card-btn">
-            <Download size={15} className="text-base" />{" "}
-            <span className="text-sm font-medium">Download</span>
+          <button
+            disabled={loading}
+            className="card-btn"
+            onClick={handleDownload}
+          >
+            {loading ? (
+              <>
+                <LoaderCircle className="w-4 h-4 animate-spin" />
+                Downloading...
+              </>
+            ) : (
+              <>
+                <Download size={15} className="text-base" />{" "}
+                <span className="text-sm font-medium">Download</span>
+              </>
+            )}
           </button>
         </div>
       </div>
