b91589ae2b3da72817cb7cc27dcb3f9d5d8664f3
Samuel Lopes <samuellopes12390@gmail.com>
2025-10-13 16:20:05 -0300

    [Money_Manager] feat(utils-apiEndpoints): Adicionado Novas Rotas de Income para Requisições à API do Backend

--- Money_Manager/frontend/src/utils/apiEndpoints.js
+++ Money_Manager/frontend/src/utils/apiEndpoints.js
@@ -7,8 +7,13 @@ export const API_ENDPOINTS = {
   REGISTER: "/register",
   GET_USER_INFO: "/profile",
   GET_ALL_CATEGORIES: "/categories",
+  CATEGORY_BY_TYPE: (type) => `/categories/${type}`,
   ADD_CATEGORY: "/categories",
   UPDATE_CATEGORY: (categoryId) => `/categories/${categoryId}`,
   GET_ALL_INCOMES: "/incomes",
+  ADD_INCOME: "/incomes",
+  DELETE_INCOME: (incomeId) => `/incomes/${incomeId}`,
+  INCOME_EXCEL_DOWNLOAD: "/excel/download/income",
+  EMAIL_INCOME: "/email/income-excel",
   UPLOAD_IMAGE: `https://api.cloudinary.com/v1_1/${CLOUDINARY_CLOUD_NAME}/image/upload`,
 };
