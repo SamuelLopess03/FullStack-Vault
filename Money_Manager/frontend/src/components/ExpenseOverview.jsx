756bed403dcf4ce2474f26a8ce261001aa62bc51
Samuel Lopes <samuellopes12390@gmail.com>
2025-10-19 18:57:23 -0300

    [Money_Manager] feat(components): Iniciado o Componente de ExpenseOverview
    O mesmo deve apresentar um gráfico e permitir a adição
    de novos expenses por meio de um botão interno.

---
+++ Money_Manager/frontend/src/components/ExpenseOverview.jsx
@@ -0,0 +1,40 @@
+import { Plus } from "lucide-react";
+import { useEffect, useState } from "react";
+
+const ExpenseOverview = ({ transactions, onAddExpense }) => {
+  const [chartData, setChartData] = useState([]);
+
+  useEffect(() => {
+    // const result = prepareIncomeLineChartData(transactions);
+
+    // console.log(result);
+    // setChartData(result);
+
+    return () => {};
+  }, [transactions]);
+
+  return (
+    <div className="card p-5 mt-5">
+      <div className="flex items-center justify-between">
+        <div>
+          <h5 className="text-lg">Expense Overview</h5>
+
+          <p className="text-xs text-gray-400 mt-0.5">
+            Track your earnings over time and analyze your expense trends.
+          </p>
+        </div>
+
+        <button
+          className="add-btn flex justify-center items-center gap-2 cursor-pointer"
+          onClick={onAddExpense}
+        >
+          <Plus size={15} className="text-lg" /> Add Expense
+        </button>
+      </div>
+
+      <div className="mt-10">{/* <CustomLineChart data={chartData} /> */}</div>
+    </div>
+  );
+};
+
+export default ExpenseOverview;
